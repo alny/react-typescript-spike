98cb3820a3936470f31f38e0c3179632cd551484
drixmix <dbthechain@gmail.com>
2018-11-18 18:40:42 +0100

    Yolo

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,110 @@
 import * as React from "react";
+import {
+  Grid,
+  Paper,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  withStyles
+} from "@material-ui/core";
 
-interface Props {}
+interface Props {
+  classes: any;
+}
+
+const styles = {
+  card: {
+    minWidth: 275,
+    margin: 50
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+};
 
 class Index extends React.Component<Props> {
-  componentDidMount() {}
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h6>Home</h6>
-      </div>
+      <Grid container spacing={24}>
+        <Grid item xs={4}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="h2">
+                be lent
+              </Typography>
+              <Typography color="textSecondary">adjective</Typography>
+              <Typography component="p">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="h2">
+                be lent
+              </Typography>
+              <Typography color="textSecondary">adjective</Typography>
+              <Typography component="p">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="h2">
+                be lent
+              </Typography>
+              <Typography color="textSecondary">adjective</Typography>
+              <Typography component="p">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default Index;
+export default withStyles(styles)(Index);
